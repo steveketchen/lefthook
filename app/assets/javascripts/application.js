1ee790d2e65c43b1bb42a2cac0dab8e03469d0b9
Steve Ketchen <steve@steveketchen.com>
2017-03-08 20:32:48 -0500

    bundle updating sqlite3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-require "sqlite"
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks

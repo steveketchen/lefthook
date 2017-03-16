f26b928896027c4873e2218d376d6dfcaa0c3c2f
Steve Ketchen <steve@steveketchen.com>
2017-03-16 18:35:39 -0400

    hopefully fixed that effing domain iframe redirect bug

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,10 @@
 require_relative 'boot'
 
+
+# changes iframe rules to allow for custom domain redirects
 config.action_dispatch.default_headers = {
     'X-Frame-Options' => 'ALLOWALL'
 }
-# changes iframe rules to allow for custom domain redirects
 
 require 'rails/all'
 
